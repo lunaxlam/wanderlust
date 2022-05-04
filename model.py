9754fea897b59f633f91c8cc3c94f2bbe9c89588
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-05-04 15:23:29 -0700

    Added class methods

--- model.py
+++ model.py
@@ -13,15 +13,15 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)                             # hashing when creating a User instance 
-    username = db.Column(db.String(20), unique=True)
-    fname = db.Column(db.String(50))
-    lname = db.Column(db.String(50))
-    current_locale = db.Column(db.String(50))
-    current_territory = db.Column(db.String(50))
-    current_country = db.Column(db.String(50))
-    about_me = db.Column(db.Text)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)                             # hashing when creating a User instance 
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+    locale = db.Column(db.String(50), nullable=False)
+    territory = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    about_me = db.Column(db.Text, nullable=True)
 
     # followers = a list of Follower objects that are following a user; use User.query.filter(User.followers).all(), for example
     # users_followed = a list of Follower objects that are users being followed; use User.query.filter(User.users_followed).all(), for example
@@ -31,6 +31,34 @@ class User(db.Model):
         """A string representation of a User."""
 
         return f"<User user_id={self.user_id} username={self.username} fname={self.fname} lname={self.lname} email={self.email} >"
+
+    @classmethod
+    def create_user(cls, email, password, username, fname, lname, locale, territory, country, about_me):
+        """Create and return a new user"""
+
+        user = User(email=email, 
+                    password=password, 
+                    username=username, 
+                    fname=fname, 
+                    lname=lname, 
+                    locale=locale, 
+                    territory=territory, 
+                    country=country, 
+                    about_me=about_me)
+        
+        return user
+
+    @classmethod
+    def get_users(cls):
+        """Return all users"""
+
+        return db.session.query(User)
+
+    @classmethod
+    def get_user_by_username(cls, username): #Need to fix
+        """Return a user by username"""
+
+        return User.query.filter(User.username == username).first()
 
 
 class Follower(db.Model):
@@ -46,11 +74,31 @@ class Follower(db.Model):
     follower = db.relationship("User", foreign_keys=[follower_id], backref="followers")
     user_followed = db.relationship("User", foreign_keys=[user_followed_id], backref="users_followed")
 
-
     def __repr__(self):
         """A string representation of a Follower."""
 
         return f"<Follower activity_id={self.activity_id} follower_id={self.follower_id} follower={self.follower.username} user_followed_id={self.user_followed_id} user_followed={self.user_followed.username}>"
+
+    @classmethod
+    def create_follower(cls, follower_id, user_followed_id):
+        """Create and return a follower"""
+
+        follower = Follower(follower_id=follower_id,
+                            user_followed_id=user_followed_id)
+        
+        return follower
+
+    @classmethod
+    def get_followers_by_user_followed_id(cls, user_followed_id):
+        """Return all followers that are following a specified user_followed_id"""
+
+        return Follower.query.filter(Follower.user_followed_id == user_followed_id).all()
+
+    @classmethod
+    def get_following_by_follower_id(cls, follower_id):
+        """Return all followers that are being followed by a specified follower_id"""
+
+        return Follower.query.filter(Follower.follower_id == follower_id).all()
 
 
 class Itinerary(db.Model):
@@ -60,8 +108,8 @@ class Itinerary(db.Model):
 
     itinerary_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    itinerary_name = db.Column(db.String)
-    overview = db.Column(db.Text)
+    itinerary_name = db.Column(db.String, nullable=False)
+    overview = db.Column(db.Text, nullable=False)
 
     # items = a list of itinerary Item objects
 
@@ -72,6 +120,28 @@ class Itinerary(db.Model):
         """A string representation of a travel itinerary."""
 
         return f"<Itinerary itinerary_id={self.itinerary_id} itinerary_name={self.itinerary_name} user_id={self.user_id} >"
+    
+    @classmethod
+    def create_itinerary(cls, user_id, itinerary_name, overview):
+        
+        itinerary = Itinerary(user_id=user_id,
+                                itinerary_name=itinerary_name,
+                                overview=overview)
+        
+        return itinerary
+
+    @classmethod
+    def get_itinerary_by_itinerary_id(cls, itinerary_id):
+        """Return an itinerary by itinerary_id"""
+
+        return Itinerary.query.filter(Itinerary.itinerary_id == itinerary_id).first()
+
+    @classmethod
+    def get_itinerary_by_user_id(cls, user_id):
+        """Return all itineraries by user_id"""
+
+        return Itinerary.query.filter(Itinerary.user_id == user_id).all()
+
 
 class Item(db.Model):
     """An itinerary item."""
@@ -80,15 +150,16 @@ class Item(db.Model):
 
     item_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     itinerary_id = db.Column(db.Integer, db.ForeignKey("itineraries.itinerary_id"))
-    item_name = db.Column(db.String(50))
-    date = db.Column(db.Date)
-    start_time = db.Column(db.String)
-    end_time = db.Column (db.String)
-    description = db.Column(db.Text)
-    locale = db.Column(db.String)
-    territory = db.Column(db.String)
-    country = db.Column(db.String)
-    place_id = db.Column(db.String)
+    item_name = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    start_time = db.Column(db.String, nullable=True)
+    end_time = db.Column (db.String, nullable=True)
+    name_or_description = db.Column(db.Text, nullable=False)
+    address = db.Column(db.String, nullable=True)
+    locale = db.Column(db.String, nullable=True)
+    territory = db.Column(db.String, nullable=True)
+    country = db.Column(db.String, nullable=True)
+    place_id = db.Column(db.String, nullable=False)
 
     # Establish a relationship between Item class and Itinerary class
     itinerary = db.relationship("Itinerary", backref="items")
@@ -97,6 +168,30 @@ class Item(db.Model):
         "A string representation of an itineary item."
 
         return f"<Item item_id={self.item_id} itinerary_id={self.itinerary_id} item_name={self.item_name} date={self.date} start_time={self.start_time} end_time={self.end_time} >"
+
+    @classmethod
+    def create_item(cls, itinerary_id, item_name, date, start_time, end_time, name_or_description,
+                    address, locale, territory, country, place_id):
+        
+        item = Item(itinerary_id=itinerary_id,
+                    item_name=item_name,
+                    date=date,
+                    start_time=start_time,
+                    end_time=end_time,
+                    name_or_description=name_or_description,
+                    address=address,
+                    locale=locale,
+                    territory=territory,
+                    country=country,
+                    place_id=place_id)
+        
+        return item
+    
+    @classmethod
+    def get_items_by_itinerary_id(cls, itinerary_id):
+        """Return all items by itinerary_id"""
+
+        return Item.query.filter(Item.itinerary_id == itinerary_id).all()
 
 
 # Set-up project
