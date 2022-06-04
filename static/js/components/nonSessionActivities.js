c25ec0a687d74d2df6115e297f6a738cb9e0f46b
lunaxlam@gmail.com <lunalam@Lunas-MBP.attlocal.net>
2022-06-03 17:15:12 -0700

    Created separate Activities.js for custom view depending on user view

---
+++ static/js/components/nonSessionActivities.js
@@ -0,0 +1,109 @@
+'use strict';
+
+function Activity( { dates, start, end, location, url, formattedAddress, phone, notes, activityID, itineraryID, placeID}) {
+
+    const edit_url = `/itinerary/${itineraryID}/edit_activity/${activityID}/${placeID}`
+
+
+    const onClick = (evt) => {
+        const clicked_button = evt.target;
+
+        const activity_id = clicked_button.getAttribute("value");
+
+        const queryString = new URLSearchParams({activity_id}).toString();
+
+        fetch(`/api/delete_activity?${queryString}`)
+            .then((response) => response.json())
+            .then((data) => {
+                alert(data.status);
+                document.location.reload();
+            })
+        .catch(() => {
+            alert('Something went wrong! Unable to delete activity.')
+        })
+    }
+
+    if (notes === '') {
+
+        return (
+            <ul>
+                <li className='main-heading'>{location} </li>    
+                <li className='main-heading'>{dates} </li> 
+                <li>Start: {start} </li> 
+                <li>End: {end} </li>       
+                <li>Address: <a href={url} target='_blank' rel='noopener noreferrer'>{formattedAddress}</a></li>
+                <li>Phone: {phone} </li>
+            </ul>
+        );
+
+    } else {
+
+        return (
+            <div className="container saved-activity">
+                <ul>
+                    <li className="main-heading">{location} </li>    
+                    <li className="main-heading">{dates} </li> 
+                    <li>Start: {start} </li> 
+                    <li>End: {end} </li>       
+                    <li>Address: <a href={url} target='_blank' rel='noopener noreferrer'>{formattedAddress}</a></li>
+                    <li>Phone: {phone} </li>
+                    <li>Notes: {notes} </li>
+                </ul>
+            </div>
+        );
+
+    } 
+}
+
+function ActivitiesContainer() {
+    
+    const [activities, setActivities] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('/api/saved_activities')
+        .then((res) => res.json())
+        .catch(() => {
+            alert('Something wrong with the route!')
+        })
+        .then((data) => {
+
+            if ('0' in data) {
+                setActivities(data)
+            }
+    
+        })
+    }, [])
+
+    const allActivities = []
+
+    for (const i in activities) {
+
+        let phone = activities[i]['results']['formatted_phone_number'];
+
+        if (typeof phone === 'undefined') {
+            phone = 'Not listed.'
+        }
+
+        allActivities.push(
+            <Activity 
+                key={i}
+                location={activities[i]['results']['name']}
+                dates={activities[i]['dates']}
+                start={activities[i]['start']}
+                end={activities[i]['end']}
+                url={activities[i]['results']['url']}
+                formattedAddress={activities[i]['results']['formatted_address']}
+                phone={phone}
+                notes={activities[i]['notes']}
+                activityID={activities[i]['activity_id']}
+            />
+        )
+    }
+    return (
+        <React.Fragment>
+            {allActivities}
+        </React.Fragment>
+    );
+}
+
+ReactDOM.render(<ActivitiesContainer />, document.querySelector('#saved-activities'));
